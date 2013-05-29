fff18e4e21895086d7e72dc940db2cc1d2536e32
Daniel Cheng <z@ithildin.com>
2013-05-29 13:03:28 -0700

    Add two more test cases.
    Eventually, these test cases should be moved into a JSON config.

--- main.go
+++ main.go
@@ -55,10 +55,36 @@ func main() {
 		},
 		1000)
 	runTrialAndReportStats(
-		"RAIDZ-1 storage pool with 3 disks",
+		"RAIDZ-1 2+1 storage pool",
 		func() int {
 			age := 0
 			pool := simulator.NewParityPool(3, 1, simulator.NewHardDiskDrive)
+			for pool.State() != simulator.FAILED {
+				age++
+				pool.Step()
+			}
+			return age
+		},
+		1000)
+	runTrialAndReportStats(
+		"RAIDZ-3 8+3 storage pool",
+		func() int {
+			age := 0
+			pool := simulator.NewParityPool(11, 3, simulator.NewHardDiskDrive)
+			for pool.State() != simulator.FAILED {
+				age++
+				pool.Step()
+			}
+			return age
+		},
+		1000)
+	runTrialAndReportStats(
+		"Mirrored storage pool with 2x RAIDZ-2 4+2 storage pools",
+		func() int {
+			age := 0
+			pool := simulator.NewStripedPool(2, func() simulator.Drive {
+				return simulator.NewParityPool(4, 2, simulator.NewHardDiskDrive)
+			})
 			for pool.State() != simulator.FAILED {
 				age++
 				pool.Step()
