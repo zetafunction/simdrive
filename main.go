a8b7a937e0c3e7c133b6decd6f99d999e6e16c3e
Daniel Cheng <z@ithildin.com>
2013-05-29 18:29:23 -0700

    Add some fun stats like survival brackets and standard deviation.

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/zetafunction/simdrive/simulator"
+	"math"
 	"runtime"
+	"sort"
 )
 
+// TODO: This code should belong in a separate package.
 type Task func(chan int)
 
 func runTrialAndReportStats(label string, task Task, iterations int) {
@@ -13,12 +16,35 @@ func runTrialAndReportStats(label string, task Task, iterations int) {
 	for i := 0; i < iterations; i++ {
 		go task(c)
 	}
-	totalHours := 0
+	results := make([]int, iterations)
 	for i := 0; i < iterations; i++ {
-		totalHours += <-c
+		results[i] = <-c
 	}
-	fmt.Printf("%s ran %d times. Average lifetime: %f\n",
-		label, iterations, float64(totalHours)/float64(iterations))
+	sort.IntSlice(results).Sort()
+	fmt.Printf("Results for %d iterations of %s:\n", iterations, label)
+
+	total := 0
+	for _, result := range results {
+		total += result
+	}
+	mean := float64(total) / float64(iterations)
+	fmt.Printf("  mean: %f\n", mean)
+
+	squared_difference_sum := 0.
+	for _, result := range results {
+		squared_difference_sum += math.Pow(float64(result)-mean, 2)
+	}
+	variance := squared_difference_sum / float64(iterations)
+	fmt.Printf("  stddev: %f\n", math.Sqrt(variance))
+
+	count := 0
+	for i := 1; i <= 10; i++ {
+		for results[count] < i*8760 {
+			count++
+		}
+		fmt.Printf("  %d year survival rate: %f\n",
+			i, float64(iterations-count)/float64(iterations))
+	}
 }
 
 func main() {
